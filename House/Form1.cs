b58a771814613a25255ecfe1196651f6c94ba76d
Jiho Lee Desktop <jiho0215@hotmail.com>
2015-11-29 16:36:38 -0600

    second

--- House/Form1.cs
+++ House/Form1.cs
@@ -12,7 +12,8 @@ namespace House
 {
     public partial class Form1 : Form
     {
-        public int numOfCom = 0;
+        public int numOfCom = 1;
+        Dictionary<string, Computer> coms = new Dictionary<string, Computer>();
 
         public Form1()
         {
@@ -31,22 +32,47 @@ namespace House
             btn1.Text = "Add a computer";
             btn1.Click += Btn1_Click;
             btn1.Parent = this;
+
+
+            int i = 5;
+            for (int z = 0; z <= 5; z++)
+            {
+                Label lbl = new Label();
+                lbl.Name = "lbl"+z;
+                lbl.Size = new Size(100, 30);
+                lbl.Location = new Point(10, 10 + 33*z);
+                lbl.Text = "Label"+z;
+                lbl.Parent = this;
+            }
+
+            MessageBox.Show("Controls[\"lbl2\"].Text"+ " is "+Controls["lbl2"].Text);
         }
 
         private void Btn1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Button Clicked");
-            Computer com = new Computer();
-            com.Name = "Com" + numOfCom;
+            //MessageBox.Show("Button Clicked");
+            //Computer com = new Computer();
+            //com.Name = "Com" + numOfCom;
+
+            coms.Add("Com" + numOfCom, new Computer());
+            MessageBox.Show(numOfCom + "  " + coms["Com1"].Type);
+            numOfCom++;
+            
         }
     }
     class Computer
     {
-        string type = "pc";
-        Label lbl = new Label();
-        lbl.Size = new Size(50, 30);
-        lbl.backColor = Color.Blue;
-        lbl.Location = new Point(30 + 60 * ( numOfCom / 4 ), 30 + 40 * ( numOfCom % 5 ) );
-        lbl.Parent = Form1;
+        private string type = "pc";
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+            //}
+            //Label lbl = new Label();
+            //lbl.Size = new Size(50, 30);
+            //lbl.backColor = Color.Blue;
+            //lbl.Location = new Point(30 + 60 * ( numOfCom / 4 ), 30 + 40 * ( numOfCom % 5 ) );
+            //lbl.Parent = Form1;
+        }
     }
 }
